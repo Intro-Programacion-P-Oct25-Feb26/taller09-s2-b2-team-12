3047f3330d93ca5411c8c0cc183652100f707eb8
teoperalta <teo.peralta.2007@gmail.com>
2025-12-11 12:24:59 -0500

    Ejercicio 03

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -11,13 +11,53 @@ package paquete02;
  */
 public class Ejercicio03 {
 
-    /**
-     * @param args the command line arguments
-     */
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
         
+   
+        double suma = 0;
+        double promedio;
+        int arriba = 0;
+        int abajo = 0;
+        
+ 
+        int longitud = arreglo.length; 
+        
+      
+        int i = 0;
+        int i2 = 0;
+        
+     
+        while (i < longitud) { 
+            suma = suma + arreglo[i];
+            i = i + 1;
+        }
+        
+      
+        promedio = suma / longitud;
+        
+      
+        while (i2 < longitud) {
+            
+         
+            if (arreglo[i2] > promedio) { 
+                arriba = arriba + 1;
+            } else if (arreglo[i2] < promedio) { 
+                abajo = abajo + 1;
+            }
+            
+           
+            i2 = i2 + 1; 
+        }
+        
+        System.out.printf("El promedio del arreglo es: %.2f\n", promedio);
+        System.out.printf("Valores por encima de la media: %d\n", arriba);
+        System.out.printf("Valores por debajo de la media: %d\n", abajo);
+        
+ 
     }
-
 }
+
