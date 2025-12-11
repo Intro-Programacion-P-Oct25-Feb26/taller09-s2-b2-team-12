3303332887e39c460ac5cd920da1f1f726c94b53
Daniel <danielp4p1@gmail.com>
2025-12-11 12:46:45 -0500

    commit

--- EjerciciosT9/src/paquete02/Ejercicio07.java
+++ EjerciciosT9/src/paquete02/Ejercicio07.java
@@ -17,8 +17,28 @@ public class Ejercicio07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+      Scanner entrada = new Scanner(System.in);
 
+        double[] nro = new double[10];
+        double suma = 0;
+
+        // Ingreso de números
+        System.out.println("Ingrese 10 números:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            nro[i] = entrada.nextDouble();
+            suma += nro[i];
+        }
+
+        // Cálculo del promedio
+        double promedio = suma / 10;
+
+        // Mostrar cada número y su diferencia
+        System.out.println("\nPROMEDIO = " + promedio);
+        System.out.println("\nNúmero\tDiferencia (número - promedio)");
+        for (int i = 0; i < 10; i++) {
+            double diferencia = nro[i] - promedio;
+            System.out.printf("%.2f\t%.2f\n", nro[i], diferencia);
+        }
     }
-
 }
