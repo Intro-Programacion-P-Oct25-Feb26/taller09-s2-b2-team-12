21f81a0b4ad47b0d656cd64551328bf21854bb60
Daniel <danielp4p1@gmail.com>
2025-12-11 11:48:18 -0500

    commit

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -21,6 +21,25 @@ public class Ejercicio04 {
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
+        int valor = promedios.length;
+        for (int i = 0; i < valor; i++) {
+            if (promedios[i] >= 0  && promedios[i] <= 5) {
+            promediosCualitativos[i] = String.format("%s promedio: %.2f "
+                    + "promedio cualitativo regular\n", 
+                    estudiantes[i], promedios [i]);
+            }
+            if (promedios[i] >= 6  && promedios[i] <= 8.9) {
+            promediosCualitativos[i] = String.format("%s promedio: %.2f "
+                    + "promedio cualitativo bueno\n", 
+                    estudiantes[i], promedios [i]);
+            }
+            if (promedios[i] >= 9  && promedios[i] <= 10) {
+            promediosCualitativos[i] = String.format("%s promedio: %.2f "
+                    + "promedio cualitativo sobresaliente\n", 
+                    estudiantes[i], promedios [i]);
+            }
+            System.out.printf("%s", promediosCualitativos[i]);
+        }
         
     }
 
